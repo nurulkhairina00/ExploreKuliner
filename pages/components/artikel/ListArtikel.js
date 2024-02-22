a27ba5cbb2af431542c514dc9f92e542536f6aa2
Nurul Khairina Surbakti <nurul.khairina@bisnis.com>
2024-02-22 14:22:07 +0700

    new feature : artikel - pegination artikel

--- pages/components/artikel/ListArtikel.js
+++ pages/components/artikel/ListArtikel.js
@@ -1,9 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { UseDispatch, useDispatch, useSelector } from "react-redux";
 import { getListArtikel } from "../../actions/artikelAction";
 import CardArtikel from "../layout/cardArtikel";
-import Pagination from "../layout/Pagination";
-import Link from "next/link";
+import Pagination from "./PaginationArtikel";
 
 const ListArtikel = () => {
   const { getListArtikelResult, getListArtikelLoading, getListArtikelError } =
@@ -13,6 +12,18 @@ const ListArtikel = () => {
   useEffect(() => {
     dispatch(getListArtikel());
   }, [dispatch]);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 8;
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+
+  const displayedData = Array.isArray(getListArtikelResult)
+    ? getListArtikelResult.slice(startIndex, endIndex)
+    : [];
+
+  const handlePageChange = (page) => setCurrentPage(page);
 
   return (
     <section className="flex flex-col p-[8vw] pb-[8vw] sm:px-5 md:px-12 lg:px-20 xl:px-32 sm:pb-30 sm:pt-10">
@@ -26,7 +37,7 @@ const ListArtikel = () => {
               type="text"
               name="search"
               placeholder="Search..."
-              className="w-full py-[3vw] px-[5vw] sm:py-4 sm:px-6 bg-white rounded-full shadow-lg text-[3vw] sm:text-sm"
+              className="w-full py-[3vw] px-[5vw] sm:py-4 sm:px-6 bg-white rounded-full shadow-lg text-[3vw] sm:text-sm hover:border-0 focus:border-0"
             />
             <button className="absolute top-1/4 right-[3vw] sm:right-4 px-[4vw] py-[1vw] sm:px-6 sm:py-1 bg-secondary text-primary rounded-full text-[2.5vw] sm:text-sm font-semibold">
               Cari
@@ -37,13 +48,20 @@ const ListArtikel = () => {
       {getListArtikelResult ? (
         <>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-[6vw] sm:gap-10">
-            {getListArtikelResult.map((item) => (
+            {displayedData.map((item) => (
               <CardArtikel data={item} key={item.id} />
             ))}
           </div>
-          <div className="py-[4vw] sm:py-6">
-            <Pagination />
-          </div>
+          {getListArtikelResult.length > itemsPerPage && (
+            <div className="py-[4vw] sm:py-6">
+              <Pagination
+                totalItems={getListArtikelResult.length}
+                itemsPerPage={itemsPerPage}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+              />
+            </div>
+          )}
         </>
       ) : getListArtikelLoading ? (
         <p>Loading...</p>
---
+++ pages/components/artikel/PaginationArtikel.js
@@ -0,0 +1,141 @@
+import React from "react";
+
+const Pagination = (props) => {
+  const { totalItems, itemsPerPage, currentPage, onPageChange } = props;
+
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  const handlePrevPage = () => {
+    onPageChange(currentPage - 1);
+  };
+
+  const handleNextPage = () => {
+    onPageChange(currentPage + 1);
+  };
+
+  const renderPageNumbers = () => {
+    const pageNumbers = [];
+    const maxPageButtons = 3;
+
+    let startPage = Math.max(currentPage - Math.floor(maxPageButtons / 2), 1);
+    let endPage = Math.min(startPage + maxPageButtons - 1, totalPages);
+
+    if (endPage - startPage + 1 < maxPageButtons)
+      startPage = Math.max(endPage - maxPageButtons + 1, 1);
+
+    for (let i = startPage; i <= endPage; i++) {
+      pageNumbers.push(
+        <button
+          key={i}
+          className={`px-[3vw] text-[3.5vw] rounded-[1.5vw] sm:px-4 sm:py-1 sm:text-lg sm:rounded-lg ${
+            currentPage === i
+              ? "text-white bg-secondary"
+              : "text-secondary border-[0.5vw] sm:border-2"
+          }`}
+          onClick={() => onPageChange(i)}
+        >
+          {i}
+        </button>
+      );
+    }
+    return pageNumbers;
+  };
+
+  return (
+    <div className="flex pt-[8vw] sm:pt-10 gap-[2vw] sm:gap-3 justify-center">
+      <button
+        className={`p-[2vw] text-[4vw] rounded-[1.5vw] sm:p-2 sm:text-lg sm:rounded-lg text-black font-semibold ${
+          currentPage === 1 ? "bg-mediumGray" : "bg-secondary"
+        }`}
+        onClick={() => onPageChange(1)}
+        disabled={currentPage === 1}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="4vw"
+          height="4vw"
+          viewBox="0 0 24 24"
+          className="sm:w-[24px] sm:h-[24px]"
+        >
+          <path
+            fill="#ffffff"
+            d="m4.836 12l6.207 6.207l1.414-1.414L7.664 12l4.793-4.793l-1.414-1.414zm5.65 0l6.207 6.207l1.414-1.414L13.314 12l4.793-4.793l-1.414-1.414z"
+          />
+        </svg>
+      </button>
+      <button
+        className={`p-[2vw] text-[4vw] rounded-[1.5vw] sm:p-2 sm:text-lg sm:rounded-lg text-black font-semibold ${
+          currentPage === 1 ? "bg-mediumGray" : "bg-secondary"
+        }`}
+        onClick={handlePrevPage}
+        disabled={currentPage === 1}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="4vw"
+          height="4vw"
+          viewBox="0 0 24 24"
+          className="sm:w-[24px] sm:h-[24px]"
+        >
+          <path
+            fill="none"
+            stroke="#ffffff"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1.5"
+            d="m14 7l-5 5l5 5"
+          />
+        </svg>
+      </button>
+
+      {renderPageNumbers()}
+
+      <button
+        className={`p-[2vw] text-[3.5vw] rounded-[1.5vw] sm:p-2 sm:text-lg sm:rounded-lg text-black font-semibold ${
+          currentPage === totalPages ? "bg-mediumGray" : "bg-secondary"
+        }`}
+        onClick={handleNextPage}
+        disabled={currentPage === totalPages}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="4vw"
+          height="4vw"
+          viewBox="0 0 24 24"
+          className="sm:w-[24px] sm:h-[24px]"
+        >
+          <path
+            fill="none"
+            stroke="#ffffff"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1.5"
+            d="m10 17l5-5l-5-5"
+          />
+        </svg>
+      </button>
+      <button
+        className={`p-[2vw] text-[4vw] rounded-[1.5vw] sm:p-2 sm:text-lg sm:rounded-lg text-black font-semibold ${
+          currentPage === totalPages ? "bg-mediumGray" : "bg-secondary"
+        }`}
+        onClick={() => onPageChange(totalPages)}
+        disabled={currentPage === totalPages}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="4vw"
+          height="4vw"
+          viewBox="0 0 24 24"
+          className="sm:w-[24px] sm:h-[24px]"
+        >
+          <path
+            fill="#ffffff"
+            d="m19.164 12l-6.207-6.207l-1.414 1.414L16.336 12l-4.793 4.793l1.414 1.414zm-5.65 0L7.307 5.793L5.893 7.207L10.686 12l-4.793 4.793l1.414 1.414z"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
